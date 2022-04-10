0e15270292c4c28aa1139553949eec494c885a0f
Danniel C <dnncervantes.yahoo.com>
2022-04-10 05:00:22 -0700

    first commit

---
+++ src/main/java/com/dannielcervantes/daikishipathvariables/DaikishipathvariablesApplication.java
@@ -0,0 +1,13 @@
+package com.dannielcervantes.daikishipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikishipathvariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikishipathvariablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dannielcervantes/daikishipathvariables/DaikishipathvariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannielcervantes.daikishipathvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikishipathvariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
